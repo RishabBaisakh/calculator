95010819595c3613279ee9ab971648ee7655f6ab
Rishab Baisakh <rbaisakh@isimtl.local>
2020-09-02 22:14:54 -0400

    Logic completed

--- src/logic/evaluate.js
+++ src/logic/evaluate.js
@@ -1,4 +1,17 @@
 function evaluate(numberOne, numberTwo, operator) {
+  switch (operator) {
+    case "+":
+      return numberOne + numberTwo;
+    case "-":
+      return numberOne - numberTwo;
+    case "*":
+      numberOne * numberTwo;
+    case "÷":
+      numberOne / numberTwo;
+    case "%":
+      number * (numberTwo / 100);
+  }
+
   return null;
 }
 
